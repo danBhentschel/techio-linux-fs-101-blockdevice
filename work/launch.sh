1b31cba6527b47947a18b917602104e0ec8919b4
danBhentschel <dan@hentschels.com>
2017-08-08 12:21:47 -0400

    Start VM attempt 3

--- work/launch.sh
+++ work/launch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ln -s /vmlinuz-4.4.0-89-generic
+ln -s /initrd.img-4.4.0-89-generic
+ln -s /ubuntu.img
+ln -s /scripts.img
+
 echo "TECHIO> terminal --init /start_vm.sh"
 
 while true; do
