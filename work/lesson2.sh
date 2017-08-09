124eec2d57a59d91a67bf1b076cb4b7399f25723
Dan Hentschel <dan@hentschels.com>
2017-08-08 23:44:08 -0400

    Change scripts partition

--- work/lesson2.sh
+++ work/lesson2.sh
@@ -1,1 +1,13 @@
-lesson1.sh
+#!/bin/bash
+
+ln -s /vmlinuz-4.4.0-89-generic
+ln -s /initrd.img-4.4.0-89-generic
+ln -s /ubuntu.img
+ln -s lesson2.img scripts.img
+truncate --size=2147483648 /ubuntu.img
+
+echo "TECHIO> terminal --init /start_vm.sh"
+
+while true; do
+    sleep 10
+done
